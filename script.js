6ea0f9bd0498b901907e9dae36b3e8a09a166fea
Moshiur Rahman <rmoshiur705@gmail.com>
2019-12-07 12:14:28 +0600

    score updated and added necessary comment

--- script.js
+++ script.js
@@ -1,64 +1,69 @@
-var playing = false;
-var score;
-var trialsLeft;
+var playing = false; //check is playing or not
+var score; //global score for updating score
+var trialsLeft; // Count trial
+// Random fruits name array
 var randomFruits = ['apple','banana','cherries','grapes','mango','orange','peach','pear','watermelon'];
-var step;
-var action;
+var step; //count ste time
+var action; //action to move fruits
 $(function(){
   // check if playing or starting
     $('#startreset').click(function(){
+      //check is playing or not
        if(playing){
           location.reload();
 
        }else{
 
          playing = true;
-         score = 0;
-         $('#scoreValue').html(score);
+         score = 0; //init score before start
+         $('.scoreValue').html(score); //update scorebox
 
-         $('#trialsLeft').show();
-          trialsLeft = 3;
+         $('#trialsLeft').show(); //show trial
+          trialsLeft = 3; //set number of trial
 
-          addHeart();
+          addHeart(); //function to add heart icon
 
-          $('#gameOver').hide();
+          $('#gameOver').hide(); //hide gameover banner when playing
 
-          $('#startreset').html('Reset Game');
+          $('#startreset').html('Reset Game'); //show reset text to button
 
-          startFruitAction();
+          startFruitAction(); //function for main gameplay
        }     
     });
 
-
+// mouseover event to detect slice
     $('#fruit1').mouseover(function() {
-      score++;
-      $('#scoreValue').html(score);
+      score++; //increment score
 
-      $('#slicesound')[0].play();
+      $('.scoreValue').html(score); //update score
 
-      clearInterval(action);
-      $('#fruit1').hide('explode', 500)
+      $('#slicesound')[0].play(); //play audio when slice
 
-      setTimeout(startFruitAction , 500);
+      clearInterval(action); //clear action
+      $('#fruit1').hide('explode', 500); //hide after slice
+
+      setTimeout(startFruitAction , 500); //action start for 500 ms
     })
 
+    //add icon for trial
 function addHeart() {
-  $('#trialsLeft').empty();
+  $('#trialsLeft').empty();//emtry trial befor append
   for(i=0; i< trialsLeft; i++){
     $('#trialsLeft').append("<img src='image/heart.png' class='heart'/>");
   }
 }
 
 function startFruitAction() {
-  $('#fruit1').show();
+  $('#fruit1').show(); 
 
-  chooseFruit();
+  chooseFruit(); //choose rando fruit
+  //add css for set fruit position
   $('#fruit1').css({
     'left': Math.round(550*Math.random()),
     'top': -50
   });
 
-  step = 1+ Math.round(5*Math.random());
+  step = 1+ Math.round(5*Math.random()); //crete step to calculate speed
 
   action = setInterval(function () {
     $('#fruit1').css('top', $('#fruit1').position().top + step);  
@@ -94,11 +99,12 @@ function startFruitAction() {
   },10);
 
 }
-
+//choose random fruit
 function chooseFruit() {
   $('#fruit1').attr('src', 'image/'+ randomFruits[Math.round(8*Math.random())] +'.png');
 }
 
+//stop action
 function stopFruitsAction() {
   clearInterval(action);
   $('#fruit1').hide();
